81ef34ebc958e39528f19994570093a86ec38617
NaveYadai <87646702+NAVEYADAI@users.noreply.github.com>
2025-06-18 19:42:17 +0300

    add docker compose

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig, loadEnv, type ConfigEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv, type ConfigEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default ({ mode }: ConfigEnv) => {
   const env = loadEnv(mode, process.cwd());
-  
+
   return defineConfig({
     plugins: [react()],
     server: {
-      port: Number(env.VITE_PORT) || 3000
-    }
-  })
-}
+      port: Number(env.VITE_PORT) || 3000,
+      host: "0.0.0.0",
+    },
+  });
+};
