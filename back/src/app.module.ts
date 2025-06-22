6a7e68c932cd61b24736f443a4e300e3352a0e17
NaveYadai <87646702+NAVEYADAI@users.noreply.github.com>
2025-06-22 07:21:15 +0300

    connect to db and can create migration

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,11 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import config from './config/config';
+
+console.log(process.env.DATABASE_URL);
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: false, // 🔴 ביטול סנכרון אוטומטי
+      migrationsRun: true, // ✅ תריץ מיגרציות אוטומטית בעת עליית השרת
+      migrations: ['dist/migrations/*.js'], // או ts אם אתה מריץ TS ישירות
+      ssl: true,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
