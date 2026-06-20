7031c3192dead573c3b67dc01be8b5d6a556b1a1
NaveYadai <87646702+NAVEYADAI@users.noreply.github.com>
2026-06-20 23:11:00 +0300

    fix up for flyio

--- back/src/main.ts
+++ back/src/main.ts
@@ -43,6 +43,6 @@ async function bootstrap() {
   app.use(bodyParser.json());
 
   const port = process.env.PORT ?? 4000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
